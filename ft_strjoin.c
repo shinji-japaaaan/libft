33b834e5fc3e0c87c2229ac31df6340b2d612c29
shinji-japaaaan <84433138+shinji-japaaaan@users.noreply.github.com>
2024-06-12 16:55:06 +0900

    ok

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -36,11 +36,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	s2_len = 0;
 	if (s1 == NULL || s2 == NULL)
 		return (NULL);
-	if (s1 != NULL)
-		s1_len = ft_strlen(s1);
-	if (s2 != NULL)
-		s2_len = ft_strlen(s2);
-	result = (char *)malloc(s1_len + s2_len + 1);
+	result = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
 	if (result == NULL)
 		return (NULL);
 	cpy(cpy(result, (char *)s1), (char *)s2);
